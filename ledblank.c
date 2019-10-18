644b2f5642ac1f20a49cdb2514eecf4db037129f
mahdyabdnia2 <54685519+mahdyabdnia2@users.noreply.github.com>
2019-10-19 00:54:16 +0330

    Add files via upload

--- ledblank.c
+++ ledblank.c
@@ -5,14 +5,35 @@
 
 void main(void)
 {
+
+DDRC.7=0x0;
+DDRC.1=0x0;
 DDRC.4 = 0x1 ;
 PORTC.4 =0x1 ;
 while (1)
 {
 // Please write your application code here
+
+if(PINC.1==0 & PINC.7==0){
+
+     
 PORTC.4 = ! PORTC.4 ;
 
-delay_ms(100) ;
+delay_ms(500) ;
+
+}
+
+
+if(PINC.1==0 & PINC.7==1){
+
+     
+PORTC.4 = ! PORTC.4 ;
+
+delay_ms(500) ;
+
+}
+
+
 
 
 }
